c2895f7bb77f11524f2dec6ff877be4023168429
urazakgul <urazdev@gmail.com>
2025-02-06 19:07:12 +0300

    added new post

---
+++ posts/en/post2/code.py
@@ -0,0 +1,8 @@
+from datafc.sofascore import match_data
+
+match_df = match_data(
+    tournament_id=17,
+    season_id=61627,
+    week_number=24,
+    enable_json_export=True
+)
---
+++ posts/tr/post2/code.py
@@ -0,0 +1,8 @@
+from datafc.sofascore import match_data
+
+match_df = match_data(
+    tournament_id=52,
+    season_id=63814,
+    week_number=22,
+    enable_json_export=True
+)
