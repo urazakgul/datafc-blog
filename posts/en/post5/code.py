ae5ca5150f5b41a20d8d881eac22851a93c797fd
urazakgul <urazdev@gmail.com>
2025-02-22 12:53:19 +0300

    added new post and simplified some previous posts

---
+++ posts/en/post5/code.py
@@ -0,0 +1,7 @@
+from datafc.sofascore import past_matches_data
+
+past_matches_df = past_matches_data(
+    tournament_id=17,
+    season_id=61627,
+    week_number=25
+)
---
+++ posts/tr/post5/code.py
@@ -0,0 +1,7 @@
+from datafc.sofascore import past_matches_data
+
+past_matches_df = past_matches_data(
+    tournament_id=52,
+    season_id=63814,
+    week_number=24
+)
